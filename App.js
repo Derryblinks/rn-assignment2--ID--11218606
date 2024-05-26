e11c44322939adaeb5bba5a0ae2f7210c3698a2f
Derryblinks <db723252@gmail.com>
2024-05-26 22:15:21 +0000

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>My name is Derrick Boateng</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#C2DFFF',
     alignItems: 'center',
     justifyContent: 'center',
+    fontSize: 24,
+    fontWeight:"bold",
+  },
+  text: {
+    fontSize: 24, // Increased font size
+    fontWeight: 'bold', // Made the name bold
   },
 });
